b17dc363b44b4980052515fbce4dda3549353bc7
fnaoto <f.naoto832+github@gmail.com>
2020-07-29 17:39:02 +0900

    Add private dns for database.

---
+++ dns.tf
@@ -0,0 +1,20 @@
+resource "google_dns_managed_zone" "private" {
+  name       = "${var.name}-private"
+  dns_name   = "private.internal."
+  visibility = "private"
+
+  private_visibility_config {
+    networks {
+      network_url = google_compute_network.network.id
+    }
+  }
+}
+
+resource "google_dns_record_set" "database" {
+  name         = "database.${google_dns_managed_zone.private.dns_name}"
+  managed_zone = google_dns_managed_zone.private.name
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = [google_sql_database_instance.database.public_ip_address]
+}
