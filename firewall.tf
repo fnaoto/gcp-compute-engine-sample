ae0dceef528448e446ef1b6e52c727aa48617dc1
fnaoto <f.naoto832+github@gmail.com>
2020-07-29 12:21:33 +0900

    Add internal firewall.

--- firewall.tf
+++ firewall.tf
@@ -10,3 +10,25 @@ resource "google_compute_firewall" "from_all" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = [google_compute_instance.instance.tags]
 }
+
+resource "google_compute_firewall" "from_internal" {
+  name    = "${var.name}-firewall-from_internal"
+  network = google_compute_network.network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = [var.public_subnet_ip_cidr_range]
+  target_tags   = [google_compute_instance.instance.tags]
+}
